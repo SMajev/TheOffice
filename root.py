87b1fa5addae3ddbd269f001e55123f243765637
smajev <smajev@gmail.com>
2021-08-01 18:18:49 +0200

    yoyo

--- root.py
+++ root.py
@@ -1,12 +1,16 @@
-from dataclasses import dataclass
 from worker import Worker
 import pickle
+import datetime
 
 
 class TheOffice:
     def __init__(self, company_name):
         self.company_name = company_name
         self.all_of_them = self.load_workers_from_file()
+        self.count_salary_for_everyone()
+        self.now = datetime.datetime.now()
+        self.safe_logs_to_file()
+        print(f"Time: {self.now}")
 
     def __repr__(self):
         return f"Company: {self.company_name},\nWorkers:\n{self.all_of_them}"
@@ -19,11 +23,19 @@ class TheOffice:
         for worker in self.all_of_them:
             print(worker)
 
+    def show_contact_worker(self, worker_name, worker_surname):
+        for worker in self.all_of_them:
+            if worker.name == worker_name:
+                if worker.surname == worker_surname:
+                    print(worker.show_contact())
+
     def show_contact_all_of_them(self):
         for worker in self.all_of_them:
-            worker_contact_string = f"*     Name: {worker.name}, Surname: {worker.surname},\n" \
-                                    f"      Phone number: {worker.phone_number}, email: {worker.email}\n"
-            print((worker_contact_string))
+            print(worker.show_contact())
+
+    def show_finance(self):
+        for worker in self.all_of_them:
+            print(worker.show_finance())
 
     def set_salary_for_worker(self, worker_name, worker_surname, value):
         for worker in self.all_of_them:
@@ -48,14 +60,6 @@ class TheOffice:
             if worker.name == worker_name:
                 if worker.surname == worker_surname:
                     worker.position = position
-
-    def show_contact_worker(self, worker_name, worker_surname):
-        for worker in self.all_of_them:
-            if worker.name == worker_name:
-                if worker.surname == worker_surname:
-                    worker_contact_string = f"*     Name: {worker_name}, Surname: {worker_surname},\n" \
-                                            f"      Phone number: {worker.phone_number}, email: {worker.email}\n"
-                    return worker_contact_string
 
     def add_hour_worker(self, worker_name, worker_surname, hours):
         for worker in self.all_of_them:
@@ -82,6 +86,15 @@ class TheOffice:
             workers = pickle.load(in_file)
             return workers
 
+    def safe_logs_to_file(self):
+        with open("logs.pickle", "ab") as out_files:
+            pickle.dump(self.now, out_files)
+
+    def load_logs_from_file(self):
+        with open("logs.pickle", "rb") as in_files:
+            logs = pickle.load(in_files)
+            return f"Last log: {logs}"
+
 
 my_office = TheOffice("MROK")
 
@@ -89,15 +102,18 @@ my_office.count_salary_for_everyone()
 
 my_office.safe_workers_to_file()
 
-print("\n\n")
-
-my_office.show_workers()
+print(my_office.load_logs_from_file())
 
 print("\n\n")
-my_office.show_contact_all_of_them()
-# print(my_office.show_contact_worker("Szymon", "Majewski"))
+# my_office.show_workers()
+
+
+print("\n\n")
+# my_office.show_contact_all_of_them()
+my_office.show_contact_worker("Szymon", "Majewski")
 # print(my_office.show_contact_worker("Kuba", "Andrzejuk"))
-# print(my_office.show_contact_worker("Ludwika", "Arseniuk"))
+my_office.show_contact_worker("Ludwika", "Arseniuk")
 # print(my_office.show_contact_worker("Przemek", "Puchalski"))
-
+# print(my_office)
+# my_office.show_finance()
 print("\n\n\n\n")
